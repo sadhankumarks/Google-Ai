73be0f98b715801c0b473e60b6bc34d3b4addea5
sadhankumarks <sadhankumark@gmail.com>
2024-06-14 01:12:23 +0530

    all

--- src/config/gimini.js
+++ src/config/gimini.js
@@ -1,57 +1,5 @@
 
 
-
-/*
-AIzaSyAXBHkzVk80GtnWH1HBNoiOYqsm5Oxd17s
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- *
- * See the getting started guide for more information
- * https://ai.google.dev/gemini-api/docs/get-started/node
- 
-import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } from "@google/generative-ai";
-  
-  const apiKey = "AIzaSyAXBHkzVk80GtnWH1HBNoiOYqsm5Oxd17s";
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-  });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run() {
-    const chatSession = model.startChat({
-      generationConfig,
-      // safetySettings: Adjust safety settings
-      // See https://ai.google.dev/gemini-api/docs/safety-settings
-      history: [],
-    });
-  
-    try {
-      const result = await chatSession.sendMessage(prompt);
-      console.log(result.response.text());
-    } catch (error) {
-      console.error("Error during API request:", error);
-    }
-  }
-  
-  export default run;*/
-
-
-
-
   /*
  * Install the Generative AI SDK
  *
